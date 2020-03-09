5c9a5c2313c1bfe6af468f1b5928ac1a5780c075
BATta0128 <tachanchanchan@gmail.com>
2020-03-09 22:41:02 +0900

    コマンドを複数設定する方法を学んだ VaultManagerの使い方と試運転

--- src/main/java/red/man10/batta/vaulttest/Vaulttest.java
+++ src/main/java/red/man10/batta/vaulttest/Vaulttest.java
@@ -1,14 +1,22 @@
 package red.man10.batta.vaulttest;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.UUID;
+
+
 public final class Vaulttest extends JavaPlugin {
-    VaultManager vault = null;
+    VaultManager vault = new VaultManager(this);
     @Override
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+        Player p = (Player)sender;
+        int money = 100;
         if (cmd.getName().equalsIgnoreCase("vaulttest")) {
             sender.sendMessage("コマンド実行!");
             getLogger().info("コマンド実行!(コンソール)");
@@ -17,6 +25,21 @@ public final class Vaulttest extends JavaPlugin {
         if (cmd.getName().equalsIgnoreCase("vaulttest2")) {
             sender.sendMessage("コマンド実行2!");
             getLogger().info("コマンド実行2!(コンソール)");
+            return true;
+        }
+        if (cmd.getName().equalsIgnoreCase("showmoney")) {
+            sender.sendMessage("お金見たいコマンド実行!");
+            getLogger().info("お金見たいコマンド実行!(コンソール)");
+            vault.showBalance(p.getUniqueId());
+
+            return true;
+        }
+        if (cmd.getName().equalsIgnoreCase("givemoney")) {
+            sender.sendMessage("お金欲しいコマンド実行!");
+            getLogger().info("お金欲しいコマンド実行!(コンソール)");
+            vault.deposit(p.getUniqueId(), money);
+            p.getPlayer().sendMessage(ChatColor.YELLOW + "$"+ money+"受取りました");
+
             return true;
         }
 
